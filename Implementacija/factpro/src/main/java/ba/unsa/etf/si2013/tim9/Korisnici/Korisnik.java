4a210899d1ee46bce98dac538ef6f9f8d3d5870f
HumaMilisic <huma.milisic@gmail.com>
2014-05-20 17:51:48 +0200

    Korisnik klasa

---
+++ Implementacija/factpro/src/main/java/ba/unsa/etf/si2013/tim9/Korisnici/Korisnik.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.si2013.tim9.Korisnici;
+
+import java.io.Serializable;
+
+public class Korisnik implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
