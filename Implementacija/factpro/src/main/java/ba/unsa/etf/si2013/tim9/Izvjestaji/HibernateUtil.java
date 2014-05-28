1085511a5887716156c3ca634abeed7a1d76ada3
edinkrupic <edinkrupic@gmail.com>
2014-05-28 12:31:40 +0200

    Dodavanje HibernateUtil.java

---
+++ Implementacija/factpro/src/main/java/ba/unsa/etf/si2013/tim9/Izvjestaji/HibernateUtil.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.si2013.tim9.Izvjestaji;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory;
+	private static final ServiceRegistry serviceRegistry;
+
+	static {
+
+		try {
+		    Configuration cfg = new Configuration();
+		    cfg.configure();
+
+		    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+		            cfg.getProperties()).build();
+		    sessionFactory = cfg.buildSessionFactory(serviceRegistry);
+
+		} catch (Throwable ex) {
+
+			System.err.println("PROBLEEEm." + ex);
+
+			throw new ExceptionInInitializerError(ex);
+
+		}
+
+	}
+
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+
+	}
+
+}
