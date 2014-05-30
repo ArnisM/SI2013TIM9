47c832bbb7461f07bd56465512d23a8788ba5cde
bmuratovic1 <bmuratovic1@etf.unsa.ba>
2014-05-30 11:05:40 +0200

    Izmjenjena forma IzmjenaUsluga

--- Implementacija/factpro/src/main/java/ba/unsa/etf/si2013/tim9/Usluge/UslugeIzmjenaForm.java
+++ Implementacija/factpro/src/main/java/ba/unsa/etf/si2013/tim9/Usluge/UslugeIzmjenaForm.java
@@ -11,8 +11,12 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.graphics.Point;
 
 public class UslugeIzmjenaForm extends Shell {
+	private Text text;
+	private Text text_1;
 
 	/**
 	 * Launch the application.
@@ -41,6 +45,7 @@ public class UslugeIzmjenaForm extends Shell {
 	 */
 	public UslugeIzmjenaForm(Display display) {
 		super(display, SWT.SHELL_TRIM);
+		setMinimumSize(new Point(200, 47));
 		createContents();
 	}
 
@@ -54,14 +59,14 @@ public class UslugeIzmjenaForm extends Shell {
 		this.setText("Izmjena usluge");
 		
 		Group group = new Group(this, SWT.NONE);
-		group.setBounds(10, 10, 361, 313);
+		group.setBounds(10, 10, 363, 339);
 		
 		List list = new List(group, SWT.BORDER);
-		list.setBounds(26, 62, 310, 183);
+		list.setBounds(26, 31, 311, 82);
 		list.setItems(new String[] {"Servisiranje ra\u010Dunara ", "Instalacija microsoft oficce-a", "Instaliranje i pode\u0161avanje operativnog sistema", "Promjena licence"});
 		
 		Label lblOdaberiteUslugu = new Label(group, SWT.NONE);
-		lblOdaberiteUslugu.setBounds(26, 41, 105, 15);
+		lblOdaberiteUslugu.setBounds(26, 10, 105, 15);
 		lblOdaberiteUslugu.setText("Odaberite uslugu:");
 		
 		Button btnIzmjeni = new Button(group, SWT.NONE);
@@ -73,9 +78,36 @@ public class UslugeIzmjenaForm extends Shell {
 				
 			}
 		});
-		btnIzmjeni.setBounds(26, 251, 105, 50);
+		btnIzmjeni.setBounds(26, 279, 105, 50);
 		btnIzmjeni.setImage(SWTResourceManager.getImage(UslugeIzmjenaForm.class, "/images/1396674353_services.png"));
 		btnIzmjeni.setText("Izmijeni");
+		
+		Label label = new Label(group, SWT.NONE);
+		label.setText("Naziv usluge:");
+		label.setBounds(26, 133, 78, 15);
+		
+		text = new Text(group, SWT.BORDER);
+		text.setBounds(114, 130, 223, 21);
+		
+		Group group_1 = new Group(group, SWT.NONE);
+		group_1.setText("Tip usluge");
+		group_1.setBounds(26, 166, 239, 65);
+		
+		Button button_1 = new Button(group_1, SWT.RADIO);
+		button_1.setText("Servis");
+		button_1.setSelection(true);
+		button_1.setBounds(89, 24, 90, 16);
+		
+		Button button_2 = new Button(group_1, SWT.RADIO);
+		button_2.setText("Konsultacije");
+		button_2.setBounds(89, 46, 90, 16);
+		
+		Label label_1 = new Label(group, SWT.NONE);
+		label_1.setText("Cijena usluge(KM):");
+		label_1.setBounds(26, 250, 100, 15);
+		
+		text_1 = new Text(group, SWT.BORDER);
+		text_1.setBounds(139, 247, 78, 21);
 		
 		Button button = new Button(this, SWT.NONE);
 		button.addSelectionListener(new SelectionAdapter() {
